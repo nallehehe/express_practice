b15960b342154a6a6e90096c8e432e604cc59337
alex <nalleballeuwu@gmail.com>
2025-10-03 13:59:52 +0200

    created jwt authentication with the help of a medium article + old node-demo code

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+require('dotenv').config();
+const errorHandler = require('./middlewares/error');
+const authRoutes = require('./routes/auth');
+const protectedRoutes = require('./routes/protectedRoute');
+
+const app = express();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log('**CONNECTED TO MONGODB DATABASE**'))
+  .catch((err) => console.log('MongoDB connection error:', err));
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+app.use('/auth', authRoutes);
+app.use('/protected', protectedRoutes);
+
+app.use(errorHandler);
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
---
+++ services/auth.js
@@ -0,0 +1,50 @@
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const ErrorResponse = require('../middlewares/errorResponse');
+
+
+//https://dvmhn07.medium.com/jwt-authentication-in-node-js-a-practical-guide-c8ab1b432a49
+
+exports.registerUser = async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new User({ username, password: hashedPassword });
+    await user.save();
+
+    res.status(201).json({ success: true, data: user });
+  } catch (error) {
+    return next(new ErrorResponse('Registration failed', 500));
+  }
+};
+
+exports.loginUser = async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+
+    const user = await User.findOne({ username });
+    if (!user) {
+      return next(new ErrorResponse('Authentication failed', 401));
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return next(new ErrorResponse('Authentication failed', 401));
+    }
+
+    const token = jwt.sign(
+      { userId: user._id },
+      process.env.ACCESS_TOKEN_SECRET,
+      {
+        expiresIn: '1h',
+      }
+    );
+
+    res.status(200).json({ success: true, token });
+  } catch (error) {
+    return next(new ErrorResponse('Login failed', 500));
+  }
+};
